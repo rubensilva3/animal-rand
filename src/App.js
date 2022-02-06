39e21d44ebdfe89d6353fd4d2b8618a6713f62d3
Ruben Tiago Silva <rubentiago3@gmail.com>
2022-02-06 19:38:24 +0000

    Get data from API using fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+  const getData = async () => {
+    const result = await fetch(
+      "https://zoo-animal-api.herokuapp.com/animals/rand"
+    );
+    const jsonResult = await result.json();
+    setData(jsonResult);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img className="wrapper" src={data.image_link} alt="img" />
+      <button
+        className="button"
+        onClick={() => {
+          getData();
+        }}
+      >
+        New animal
+      </button>
+      <h1>{data.name}</h1>
+      <p>
+        <b>Latin name: </b>
+        {data.latin_name}
+      </p>
+      <p>
+        <b>Animal type: </b>
+        {data.animal_type}
+      </p>
+      <p>
+        <b>Active time: </b>
+        {data.active_time}
+      </p>
+      <p>
+        <b>Min length: </b>
+        {data.length_min}
+      </p>
+      <p>
+        <b>Max length: </b>
+        {data.length_max}
+      </p>
+      <p>
+        <b>Min weight: </b>
+        {data.weight_min}
+      </p>
+      <p>
+        <b>Max weight: </b>
+        {data.weight_max}
+      </p>
+      <p>
+        <b>Lifespan: </b>
+        {data.lifespan}
+      </p>
+      <p>
+        <b>Habitat: </b>
+        {data.habitat}
+      </p>
+      <p>
+        <b>Diet: </b>
+        {data.diet}
+      </p>
+      <p>
+        <b>Geographic range: </b>
+        {data.geo_range}
+      </p>
     </div>
   );
 }
